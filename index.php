504416d317433cf1a7e22304b1fe45d59f274290
Marcin Hanckowiak <marcinhanckowiak@wp.pl>
2023-03-28 17:38:35 +0200

    Stan pracy na 17:38 / 28.03.2023

--- index.php
+++ index.php
@@ -4,10 +4,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="styl.css">
     <title>Sklep</title>
+   
+    <!-- <style>
+ #btn{
+    margin-top: 10px;
+}
+    </style> -->
 </head>
 <body>
+    <div id="panel">
+        <form action="index.php" method="POST">
+            Nazwa uzytkownika: <br>
+            <input type="text" name="login" id="login" > <br>
+            Haslo:<br>
+            <input type="password" name="password" id="password"> <br>
+            <button type="submit" id="btn">Zaloguj</button>
+        </form>
+    </div>
     
 </body>
 </html>
