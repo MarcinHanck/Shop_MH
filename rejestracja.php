c8131f76fe6d1f833da0ba0fc8488a85ea42cf94
Marcin Hanckowiak <marcinhanckowiak@wp.pl>
2023-03-29 13:32:35 +0200

    git add -A

--- rejestracja.php
+++ rejestracja.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="styl-index.css">
+    <link rel="stylesheet" type="text/css" href="styl-rejest.css">
     <title>Sklep</title>
    
   
 </head>
 <body>
-    <p>Logowanie</p>
+    <p>Rejestracja </p>
 
     <div id="panel">
         <form action="login.php" method="POST" id="form">
@@ -19,8 +19,10 @@
             <input type="text" name="login" id="login" > <br>
             <a>Haslo:</a><br>
             <input type="password" name="password" id="password"> <br>
-            <button type="submit" id="btn">Zaloguj</button> <br>
-            <a href="rejestracja.php" id="rejestracja">Rejestracja</a>
+            <a>Adres e-mail:</a><br>
+            <input type="text" name="email" id="email"> <br>
+            <button type="submit" id="btn">Zarejestruj się</button> 
+            
             
         </form>
     </div>
